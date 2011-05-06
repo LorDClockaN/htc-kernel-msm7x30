0b32d10d6ab571b23b61c668eaa27d820b5511d2
LorD ClockaN <davor@losinj.com>
2011-05-06 20:23:26 +0200

    arm/mach-msm/htc_acoustic_7x30: cleanups

--- arch/arm/mach-msm/htc_acoustic_7x30.c
+++ arch/arm/mach-msm/htc_acoustic_7x30.c
@@ -464,7 +464,9 @@ int enable_mic_bias(int on)
 static int __init acoustic_init(void)
 {
 	int ret = 0;
-
+#if defined(CONFIG_HTC_HEADSET_MGR)
+	struct headset_notifier notifier;
+#endif
 	mutex_init(&api_lock);
 	mutex_init(&rpc_connect_lock);
 	ret = misc_register(&acoustic_misc);
@@ -493,7 +495,6 @@ static int __init acoustic_init(void)
 
 #if defined(CONFIG_HTC_HEADSET_MGR)
 	{
-		struct headset_notifier notifier;
 		notifier.id = HEADSET_REG_MIC_BIAS;
 		notifier.func = enable_mic_bias;
 		headset_notifier_register(&notifier);
